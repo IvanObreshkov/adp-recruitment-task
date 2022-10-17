c88331d0ca24997fb2a56082a58f66709860cb4a
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2022-10-17 20:28:47 +0300

    Add sqlite

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
-import scrapy, w3lib, re, typing
+import scrapy, w3lib, re, typing, json
 from datetime import datetime
+import sqlite3
 
 
 def clear_html_tags(scraped_list: list) -> list:
@@ -33,13 +34,58 @@ def generate_id(start_urls: list):
         id_of_article = id_of_article + 1
         print(id_of_article)
 
+def connect_db():
+    con = sqlite3.connect("articles.db")
+    cursor = con.cursor()
+    create_table(con, cursor)
+    return con, cursor
+
+def create_table(con, cursor):
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS article (
+                                            item_id integer PRIMARY KEY,
+                                            url text NOT NULL,
+                                            article_date text NOT NULL,
+                                            labels text NOT NULL,
+                                            links text integer NOT NULL,
+                                            body TEXT NOT NULL
+                                        );"""
+
+    try:
+        cursor.execute(sql_create_tasks_table)
+        con.commit()
+    except Exception as e:
+        print(e)
 
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
     info = {}
     start_urls = ['https://nbs.sk/en/news/nbs-warning-about-axe-capital-group-se/',
-                  'https://nbs.sk/en/news/statement-from-the-nbs-bank-boards-17th-meeting-of-2022/']
+                  'https://nbs.sk/en/news/statement-from-the-nbs-bank-boards-17th-meeting-of-2022/',
+                  'https://nbs.sk/en/news/statement-from-the-nbs-bank-boards-18th-meeting-of-2022%ef%bf%bc/',
+                  'https://nbs.sk/en/news/statement-from-the-nbs-bank-boards-16th-meeting-of-2022/',
+                  'https://nbs.sk/en/news/nbs-warning-about-axe-capital-group-se/',
+                  'https://nbs.sk/en/news/nbs-warning-about-feb-bank/',
+                  'https://nbs.sk/en/news/statement-from-the-nbs-bank-boards-12th-meeting-of-2022/',
+                  'https://nbs.sk/en/news/statement-from-the-nbs-bank-boards-9th-meeting-of-2022/',
+                  'https://nbs.sk/en/news/statement-from-the-1st-meeting-of-the-bank-board-of-the-nbs/',
+                  'https://nbs.sk/en/news/nbs-warning-about-salus-populi/',
+                  'https://nbs.sk/en/news/nbs-warning-about-proof-of-no-debt-documents-provided-through-the-website-of-the-central-register-of-debtors-of-the-slovak-republic-cerd/',
+                  'https://nbs.sk/en/news/novis-insurance-company/',
+                  'https://nbs.sk/en/news/nbs-warning-about-the-activities-of-ci-investment-group-s-r-o-operator-of-the-platform-www-investiciaslovensko-sk/',
+                  'https://nbs.sk/en/news/nbs-warning-about-the-activities-of-world-agency-s-r-o-operator-of-the-website-www-worldagency-sk/',
+                  'https://nbs.sk/en/news/nbs-warns-about-misleading-advertisements-for-alternative-bank-accounts/',
+                  'https://nbs.sk/en/news/coronavirus-postponement-of-start-of-sales-of-e10-silver-collector-coin-andrej-sladkovic/',
+                  'https://nbs.sk/en/news/nbs-announces-temporary-closure-of-public-counters-and-filing-office/',
+                  'https://nbs.sk/en/news/nbs-launches-research-archive-portal/',
+                  'https://nbs.sk/en/news/notice-of-the-commencement-of-winding-up-proceedings-against-lamp-insurance-company-limited/',
+                  'https://nbs.sk/en/news/nbs-warning-what-is-a-pyramid-scheme-airplane-game-or-ponzi-scheme/'
+                  ]
+
     article_id: int = 0
+
+    con, cursor = connect_db()
+
+
 
     def parse(self, response):
         article_labels: list = clear_html_tags(
@@ -52,12 +98,16 @@ class BlogSpider(scrapy.Spider):
         article_links: list = re.findall(
             "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})",
             paragraphs)
-        self.article_id = self.article_id + 1;
+        self.article_id = self.article_id + 1
 
-        with open('file.txt', 'a') as file:
-            file.write("\nitem_id: {}".format(self.article_id))
-            file.write("\nurl: {}".format(self.start_urls[self.article_id-1]))
-            file.write("\ndate: {}".format(date))
-            file.write("\nlabels: {}".format(article_labels))
-            file.write("\nlinks: {}".format(str(article_links)))
-            file.write("\nbody: {}".format(paragraphs))
+        schema = {
+            "item_id": self.article_id,
+            "url": self.start_urls[self.article_id - 1],
+            "article_date": date,
+            "labels": str(article_labels),
+            "links": str(article_links),
+            "body": paragraphs
+        }
+
+        self.cursor.execute("INSERT INTO article (item_id, url, article_date, labels, links, body) VALUES (:item_id, :url, :article_date, :labels, :links, :body)", schema)
+        self.con.commit()
